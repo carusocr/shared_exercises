1a61fdd248b738d226ccc03d6ac383cf726a0535
Chris Caruso <carusocr@ldc.upenn.edu>
2019-01-29 06:53:13 -0800

    Update exercise_1.py

--- exercise_1.py
+++ exercise_1.py
@@ -3,8 +3,10 @@ import sys
 def max(num1, num2):
   if num1 > num2:
     return num1
-  else:
+  elsif num2 > num1:
     return num2
+  else:
+    return "equal"
 
 num1 = sys.argv[1]
 num2 = sys.argv[2]
